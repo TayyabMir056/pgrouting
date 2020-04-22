e890edc74eaeacedaffdbc2400b3975e58a7850c
TayyabMir056 <54217923+TayyabMir056@users.noreply.github.com>
2020-04-23 03:08:04 +0400

    Added points of interest

--- sample_data.sql
+++ sample_data.sql
@@ -1,4 +1,4 @@
-CREATE TABLE edge_table (
+CREATE TABLE sample_edge_table (
     id serial,
     dir character varying,
     source integer,
@@ -13,26 +13,26 @@ CREATE TABLE edge_table (
 );
 
 
-INSERT INTO edge_table (cost,reverse_cost,x1,y1,x2,y2) VALUES ( 1, 1,  2,0,   2,1);
-INSERT INTO edge_table (cost,reverse_cost,x1,y1,x2,y2) VALUES (-1, 1,  2,1,   3,1);
-INSERT INTO edge_table (cost,reverse_cost,x1,y1,x2,y2) VALUES (-1, 1,  3,1,   4,1);
-INSERT INTO edge_table (cost,reverse_cost,x1,y1,x2,y2) VALUES ( 1, 1,  2,1,   2,2);
-INSERT INTO edge_table (cost,reverse_cost,x1,y1,x2,y2) VALUES ( 1,-1,  3,1,   3,2);
-INSERT INTO edge_table (cost,reverse_cost,x1,y1,x2,y2) VALUES ( 1, 1,  0,2,   1,2);
-INSERT INTO edge_table (cost,reverse_cost,x1,y1,x2,y2) VALUES ( 1, 1,  1,2,   2,2);
-INSERT INTO edge_table (cost,reverse_cost,x1,y1,x2,y2) VALUES ( 1, 1,  2,2,   3,2);
-INSERT INTO edge_table (cost,reverse_cost,x1,y1,x2,y2) VALUES ( 1, 1,  3,2,   4,2);
-INSERT INTO edge_table (cost,reverse_cost,x1,y1,x2,y2) VALUES ( 1, 1,  2,2,   2,3);
-INSERT INTO edge_table (cost,reverse_cost,x1,y1,x2,y2) VALUES ( 1,-1,  3,2,   3,3);
-INSERT INTO edge_table (cost,reverse_cost,x1,y1,x2,y2) VALUES ( 1,-1,  2,3,   3,3);
-INSERT INTO edge_table (cost,reverse_cost,x1,y1,x2,y2) VALUES ( 1,-1,  3,3,   4,3);
-INSERT INTO edge_table (cost,reverse_cost,x1,y1,x2,y2) VALUES ( 1, 1,  2,3,   2,4);
-INSERT INTO edge_table (cost,reverse_cost,x1,y1,x2,y2) VALUES ( 1, 1,  4,2,   4,3);
-INSERT INTO edge_table (cost,reverse_cost,x1,y1,x2,y2) VALUES ( 1, 1,  4,1,   4,2);
-INSERT INTO edge_table (cost,reverse_cost,x1,y1,x2,y2) VALUES ( 1, 1,  0.5,3.5,  1.999999999999,3.5);
-INSERT INTO edge_table (cost,reverse_cost,x1,y1,x2,y2) VALUES ( 1, 1,  3.5,2.3,  3.5,4);
+INSERT INTO sample_edge_table (cost,reverse_cost,x1,y1,x2,y2) VALUES ( 1, 1,  2,0,   2,1);
+INSERT INTO sample_edge_table (cost,reverse_cost,x1,y1,x2,y2) VALUES (-1, 1,  2,1,   3,1);
+INSERT INTO sample_edge_table (cost,reverse_cost,x1,y1,x2,y2) VALUES (-1, 1,  3,1,   4,1);
+INSERT INTO sample_edge_table (cost,reverse_cost,x1,y1,x2,y2) VALUES ( 1, 1,  2,1,   2,2);
+INSERT INTO sample_edge_table (cost,reverse_cost,x1,y1,x2,y2) VALUES ( 1,-1,  3,1,   3,2);
+INSERT INTO sample_edge_table (cost,reverse_cost,x1,y1,x2,y2) VALUES ( 1, 1,  0,2,   1,2);
+INSERT INTO sample_edge_table (cost,reverse_cost,x1,y1,x2,y2) VALUES ( 1, 1,  1,2,   2,2);
+INSERT INTO sample_edge_table (cost,reverse_cost,x1,y1,x2,y2) VALUES ( 1, 1,  2,2,   3,2);
+INSERT INTO sample_edge_table (cost,reverse_cost,x1,y1,x2,y2) VALUES ( 1, 1,  3,2,   4,2);
+INSERT INTO sample_edge_table (cost,reverse_cost,x1,y1,x2,y2) VALUES ( 1, 1,  2,2,   2,3);
+INSERT INTO sample_edge_table (cost,reverse_cost,x1,y1,x2,y2) VALUES ( 1,-1,  3,2,   3,3);
+INSERT INTO sample_edge_table (cost,reverse_cost,x1,y1,x2,y2) VALUES ( 1,-1,  2,3,   3,3);
+INSERT INTO sample_edge_table (cost,reverse_cost,x1,y1,x2,y2) VALUES ( 1,-1,  3,3,   4,3);
+INSERT INTO sample_edge_table (cost,reverse_cost,x1,y1,x2,y2) VALUES ( 1, 1,  2,3,   2,4);
+INSERT INTO sample_edge_table (cost,reverse_cost,x1,y1,x2,y2) VALUES ( 1, 1,  4,2,   4,3);
+INSERT INTO sample_edge_table (cost,reverse_cost,x1,y1,x2,y2) VALUES ( 1, 1,  4,1,   4,2);
+INSERT INTO sample_edge_table (cost,reverse_cost,x1,y1,x2,y2) VALUES ( 1, 1,  0.5,3.5,  1.999999999999,3.5);
+INSERT INTO sample_edge_table (cost,reverse_cost,x1,y1,x2,y2) VALUES ( 1, 1,  3.5,2.3,  3.5,4);
 
-UPDATE edge_table SET the_geom = st_makeline(st_point(x1,y1),st_point(x2,y2)),
+UPDATE sample_edge_table SET the_geom = st_makeline(st_point(x1,y1),st_point(x2,y2)),
                       dir = CASE WHEN (cost>0 and reverse_cost>0) THEN 'B'   -- both ways
                                  WHEN (cost>0 and reverse_cost<0) THEN 'FT'  -- direction of the LINESSTRING
                                  WHEN (cost<0 and reverse_cost>0) THEN 'TF'  -- reverse direction of the LINESTRING
@@ -40,17 +40,28 @@ UPDATE edge_table SET the_geom = st_makeline(st_point(x1,y1),st_point(x2,y2)),
 								 
 								 
 								 
-CREATE TABLE vertex_table (
-    id serial,
-    x double precision,
-    y double precision,
-	geom geometry
+CREATE TABLE pointsOfInterest(
+    pid BIGSERIAL,
+    x FLOAT,
+    y FLOAT,
+    edge_id BIGINT,
+    side CHAR,
+    fraction FLOAT,
+    the_geom geometry,
+    newPoint geometry
 );
 
-INSERT INTO vertex_table (id,x,y) VALUES
-        (1,2,0), (2,2,1), (3,3,1), (4,4,1), (5,0,2), (6,1,2), (7,2,2),
-        (8,3,2), (9,4,2), (10,2,3), (11,3,3), (12,4,3), (13,2,4);
-		
-UPDATE vertex_table SET "geom" = st_point(x,y) WHERE 1=1;
+INSERT INTO pointsOfInterest (x, y, edge_id, side, fraction) VALUES
+(1.8, 0.4,   1, 'l', 0.4),
+(4.2, 2.4,  15, 'r', 0.4),
+(2.6, 3.2,  12, 'l', 0.6),
+(0.3, 1.8,   6, 'r', 0.3),
+(2.9, 1.8,   5, 'l', 0.8),
+(2.2, 1.7,   4, 'b', 0.7);
+UPDATE pointsOfInterest SET the_geom = st_makePoint(x,y);
 
+UPDATE pointsOfInterest
+    SET newPoint = ST_LineInterpolatePoint(e.the_geom, fraction)
+    FROM edge_table AS e WHERE edge_id = id;
+		
 		
