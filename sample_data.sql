02fd317d592e9a080e7c1d4389791f7f8cb0e97f
TayyabMir056 <54217923+TayyabMir056@users.noreply.github.com>
2020-04-24 03:16:16 +0400

    Create topology and comments added

--- sample_data.sql
+++ sample_data.sql
@@ -14,7 +14,7 @@ CREATE TABLE sample_edge_table (
     the_geom geometry,
 	floor_id integer);
 
-
+-- INSERT FLOOR 1 EDGES
 INSERT INTO sample_edge_table (cost,reverse_cost,x1,y1,x2,y2,floor_id) VALUES ( 1, 1,  2,0,   2,1,1);
 INSERT INTO sample_edge_table (cost,reverse_cost,x1,y1,x2,y2,floor_id) VALUES (1, 1,  2,1,   3,1,1);
 INSERT INTO sample_edge_table (cost,reverse_cost,x1,y1,x2,y2,floor_id) VALUES (1, 1,  3,1,   4,1,1);
@@ -34,6 +34,7 @@ INSERT INTO sample_edge_table (cost,reverse_cost,x1,y1,x2,y2,floor_id) VALUES ( 
 INSERT INTO sample_edge_table (cost,reverse_cost,x1,y1,x2,y2,floor_id) VALUES ( 1, 1,  0.5,3.5,  1.999999999999,3.5,1);
 INSERT INTO sample_edge_table (cost,reverse_cost,x1,y1,x2,y2,floor_id) VALUES ( 1, 1,  3.5,2.3,  3.5,4,1);
 
+-- INSERT FLOOR 2 EDGES
 INSERT INTO sample_edge_table (cost,reverse_cost,x1,y1,x2,y2,floor_id) VALUES ( 1, 1,  3,1,   3,2,2);
 INSERT INTO sample_edge_table (cost,reverse_cost,x1,y1,x2,y2,floor_id) VALUES (1, 1,  3,2,   4,2,2);
 INSERT INTO sample_edge_table (cost,reverse_cost,x1,y1,x2,y2,floor_id) VALUES (1, 1,  4,2,   5,2,2);
@@ -53,14 +54,15 @@ INSERT INTO sample_edge_table (cost,reverse_cost,x1,y1,x2,y2,floor_id) VALUES ( 
 INSERT INTO sample_edge_table (cost,reverse_cost,x1,y1,x2,y2,floor_id) VALUES ( 1, 1,  1.5,4.5,  2.999999999999,4.5,2);
 INSERT INTO sample_edge_table (cost,reverse_cost,x1,y1,x2,y2,floor_id) VALUES ( 1, 1,  4.5,3.3,  4.5,5,2);
 
-
+--CREATE GEOMETRIES FROM X Y POINTS
 UPDATE sample_edge_table SET the_geom = st_makeline(st_point(x1,y1),st_point(x2,y2)),
                       dir = CASE WHEN (cost>0 and reverse_cost>0) THEN 'B'   -- both ways
                                  WHEN (cost>0 and reverse_cost<0) THEN 'FT'  -- direction of the LINESSTRING
                                  WHEN (cost<0 and reverse_cost>0) THEN 'TF'  -- reverse direction of the LINESTRING
                                  ELSE '' END;                                -- unknown
-								 
 
+
+-- CREATE POI TABLE (FIRE EXTENGUISHERS)
 DROP TABLE pointsOfInterest;
 CREATE TABLE pointsOfInterest(
     pid BIGSERIAL,
@@ -74,6 +76,7 @@ CREATE TABLE pointsOfInterest(
 	floor_id integer
 );
 
+--Floor 1 pois
 INSERT INTO pointsOfInterest (x, y, edge_id, side, fraction,floor_id) VALUES
 (1.8, 0.4,   1, 'l', 0.4,1),
 (4.2, 2.4,  15, 'r', 0.4,1),
@@ -81,7 +84,7 @@ INSERT INTO pointsOfInterest (x, y, edge_id, side, fraction,floor_id) VALUES
 (0.3, 1.8,   6, 'r', 0.3,1),
 (2.9, 1.8,   5, 'l', 0.8,1),
 (2.2, 1.7,   4, 'b', 0.7,1),
-
+--FLoor 2 pois
 (2.8, 1.4,   1, 'l', 0.4,2),
 (5.2, 3.4,  15, 'r', 0.4,2),
 (3.6, 4.2,  12, 'l', 0.6,2),
@@ -89,7 +92,13 @@ INSERT INTO pointsOfInterest (x, y, edge_id, side, fraction,floor_id) VALUES
 (3.9, 2.8,   5, 'l', 0.8,2),
 (3.2, 2.7,   4, 'b', 0.7,2);
 
+-- create geom from x y
 UPDATE pointsOfInterest SET the_geom = st_makePoint(x,y);
+
+-snap the points to nearest line of same floor and store the new geom in column newPoint
 UPDATE pointsOfInterest
     SET newPoint = ST_LineInterpolatePoint(e.the_geom, fraction)
     FROM sample_edge_table AS e WHERE edge_id = id AND pointsOfInterest.floor_id=e.floor_id;
+    
+--CREATE TOPOLOGOGY (This will fill the source and target columns of edges)
+SELECT pgr_createTopology('sample_edge_table',0.001);
