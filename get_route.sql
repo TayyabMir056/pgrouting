4a1264c676b5dce751d0ee9ca84ef1fa0a457348
TayyabMir056 <54217923+TayyabMir056@users.noreply.github.com>
2020-04-30 15:47:19 +0400

    delete old get route

--- get_route.sql
+++
@@ -1,6 +0,0 @@
-SELECT * FROM pgr_withPoints(
-        'SELECT id, source, target, cost, reverse_cost FROM sample_edge_table ORDER BY id',
-        'SELECT pid, edge_id, fraction, side from pointsOfInterest',
-        -1, --Starting point = POI 1 (- for POI)
-		3, --Ending point = Vertex 3
-        details := true);
